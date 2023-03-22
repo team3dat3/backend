abf97d0926d542d908a8981ee07ccbf1a7c8ffa9
ThomasSA-Git <thom45w9@stud.kea.dk>
2023-03-22 09:30:08 +0100

    ShowService done

--- src/main/java/com/team3dat3/backend/dto/show/ShowRequest.java
+++ src/main/java/com/team3dat3/backend/dto/show/ShowRequest.java
@@ -1,4 +1,42 @@
 package com.team3dat3.backend.dto.show;
 
+import com.team3dat3.backend.entity.Movie;
+import com.team3dat3.backend.entity.Reservation;
+import com.team3dat3.backend.entity.Show;
+import com.team3dat3.backend.entity.ShowDateTime;
+import lombok.*;
+
+import java.util.List;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
 public class ShowRequest {
+
+
+  private int showId;
+
+  private Movie movie;
+
+  private List<Reservation> reservations;
+
+  private List<ShowDateTime> showDates;
+  public void copyTo(Show show) {
+    show.setShowId(showId);
+    show.setMovie(movie);
+    show.setReservations(reservations);
+    show.setShowDates(showDates);
+  }
+
+  public static Show getShowEntity(ShowRequest request) {
+    return Show.builder()
+        .showId(request.getShowId())
+        .movie(request.getMovie())
+        .reservations(request.getReservations())
+        .showDates(request.getShowDates())
+        .build();
+  }
 }
--- src/main/java/com/team3dat3/backend/entity/Show.java
+++ src/main/java/com/team3dat3/backend/entity/Show.java
@@ -1,10 +1,7 @@
 package com.team3dat3.backend.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.List;
 
@@ -13,6 +10,7 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Show {
 
   @Id
