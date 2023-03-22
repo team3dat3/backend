f508dd46508f9ad54e7d9c8a2ac8accfff9ed283
ThomasSA-Git <thom45w9@stud.kea.dk>
2023-03-22 11:57:34 +0100

    ShowDataTime added

--- src/main/java/com/team3dat3/backend/entity/ShowDateTime.java
+++ src/main/java/com/team3dat3/backend/entity/ShowDateTime.java
@@ -3,9 +3,15 @@ package com.team3dat3.backend.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import lombok.*;
 import org.joda.time.DateTime;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ShowDateTime {
 
   @Id
