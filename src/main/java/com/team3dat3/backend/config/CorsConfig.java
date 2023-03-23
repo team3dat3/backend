c7da443f9ac513695e78994c753d22fa17537ac7
ThomasSA-Git <thom45w9@stud.kea.dk>
2023-03-23 09:11:48 +0100

    CorsConfig added

--- src/main/java/com/team3dat3/backend/config/CorsConfig.java
+++ src/main/java/com/team3dat3/backend/config/CorsConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer{
 
-
+//Not done
     @Override
     public void addCorsMappings(CorsRegistry registry) {
       registry.addMapping("/api/**")
