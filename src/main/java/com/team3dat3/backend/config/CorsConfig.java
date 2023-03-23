b97a9dd2d14d989409d22733dfb52cc060418cfb
ThomasSA-Git <thom45w9@stud.kea.dk>
2023-03-23 10:09:37 +0100

    CorsConfig fixed

--- src/main/java/com/team3dat3/backend/config/CorsConfig.java
+++ src/main/java/com/team3dat3/backend/config/CorsConfig.java
@@ -16,38 +16,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer{
 
+    private static final String[] ALLOWED_ORIGINS = new String[] {
+        "https://urchin-app-9ivwp.ondigitalocean.app/movies%22",
+        "https://www.urchin-app-9ivwp.ondigitalocean.app/movies%22",
+        "http://localhost"
+    };
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-  /*  Ville nedenstående være godt nok eller skal det være mere specifikt som det der ikke er udkommenteret
-      registry.addMapping("/api/**")
-          .allowedOrigins("https://urchin-app-9ivwp.ondigitalocean.app");*/
-
-      registry.addMapping("/api/movies/**")
-          .allowedOrigins("https://urchin-app-9ivwp.ondigitalocean.app/movies");
-      //.allowedMethods("GET"); måske ikke nødvendig..?
-
-      registry.addMapping("/api/v1/reservations/**")
-          .allowedOrigins("https://urchin-app-9ivwp.ondigitalocean.app/reservations");
-
-      registry.addMapping("/api/v1/users/**")
-          .allowedOrigins("https://urchin-app-9ivwp.ondigitalocean.app/users");
-
-      registry.addMapping("/api/v1/errors")
-          .allowedOrigins("https://urchin-app-9ivwp.ondigitalocean.app/errors");
-
-      registry.addMapping("/api/v1/coupons/**")
-          .allowedOrigins("https://urchin-app-9ivwp.ondigitalocean.app/coupons");
-
-      registry.addMapping("/api/shows/**")
-          .allowedOrigins("https://urchin-app-9ivwp.ondigitalocean.app/shows");
-
-      registry.addMapping("/api/shows/theater/**")
-          .allowedOrigins("https://urchin-app-9ivwp.ondigitalocean.app/theater");
-
-      registry.addMapping("/api/shows/seatrows/**")
-          .allowedOrigins("https://urchin-app-9ivwp.ondigitalocean.app/seatrows");
-
-      registry.addMapping("/api/shows/seats/**")
-          .allowedOrigins("https://urchin-app-9ivwp.ondigitalocean.app/seats");
+      registry.addMapping("/**")
+          .allowedOrigins(ALLOWED_ORIGINS);
+    }
   }
-}
